karolherbst/shader-db

commit ebe492010c0d3f882ac8b2b4957147d362ab439c

report: Stop hard-coding the default set of measurements.
It sucks to need to hack up report.py each time I want to change the
set of things reported by shader-db.  This also makes it so that v3d
reports work by default in reports.py.

Tested-by: Eduardo Lima Mitev <elima@igalia.com>

report.py
@@ -125,7 +125,6 @@ def mean_confidence_interval(data, confidence=0.95):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--measurements", "-m", type=split_list,
-                        default=["instructions", "cycles", "loops", "spills", "fills"],
                         help="comma-separated list of measurements to report")
     parser.add_argument("--summary-only", "-s", action="store_true", default=False,
                         help="do not show the per-shader helped / hurt data")
@@ -151,6 +150,17 @@ def main():
     helped_statistics = {}
     hurt_statistics = {}
     confidence_interval = {}
+
+    # If no set of measurements is specified, pick an arbitrary shader and use
+    # the ones it has.  The assumption is that all shaders will have had the
+    # same set of measurements printed
+    if not args.measurements:
+        args.measurements = []
+
+        for p in args.before:
+            for m in args.before[p]:
+                args.measurements.append(m)
+            break
 
     for m in args.measurements:
         total_before[m] = 0

commit 9310bba265de7bb1dd8930ddf107dc02649fcaf8

report: Fix heuristic for default set of measurements
Need to look at the results in `before`, not the filename in
`args.before`.

Fixes: ebe4920 "report: Stop hard-coding the default set of measurements."
Tested-by: Rob Clark <robdclark@chromium.org>
Reviewed-by: Eric Anholt <eric@anholt.net>

report.py
@@ -157,8 +157,8 @@ def main():
     if not args.measurements:
         args.measurements = []
 
-        for p in args.before:
-            for m in args.before[p]:
+        for p in before:
+            for m in before[p]:
                 args.measurements.append(m)
             break
 